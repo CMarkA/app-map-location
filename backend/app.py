7cc7919010a8c83085d0ad43363eb14258035529
CMarkA <marcosacostalezcano2006@gmail.com>
2024-11-28 20:45:02 -0300

    cambio

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,8 @@ import folium
 # Crear mapa centrado en una ubicación
 mapa = folium.Map(location=[-31.24735587063069, -61.51029371921464], zoom_start=16)  # Rafela, como ejemplo.
 
+#Puto el que lee
+
 # Guardar el mapa
 mapa.save("mapa.html")
 
